ec205da3b9f3258b92e2cbac71823d0335f649da
Elizabeth Duarte Ortiz <134437416+liv-ortiz25@users.noreply.github.com>
2023-10-05 16:22:09 +0000

    update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,75 +1,41 @@
-import React, { useState} from "react";
+import React, { useState, useEffect} from "react";
 
 
-const Home =() => {
-    const [task, setTasks] = useState('');       
-    const [tasksList, setTasksList] = useState([]); 
-
-    window.onload = function createUser (e) {
-        e.preventDefault();
-        console.log('crear usuario')
-        const requestOptions = {
+function Home() {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState('');
+  
+    useEffect(() => {
+      fetch('https://playground.4geeks.com/apis/fake/todos/user/elizaduarte')
+        .then((response) => response.json())
+        .then((data) => setTasks(data));
+    }, []);
+  
+    const addTask = () => {
+      if (newTask.trim() !== '') {
+        fetch('https://playground.4geeks.com/apis/fake/todos/user/elizaduarte', {
           method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify( [] )
-      };
-        fetch('https://playground.4geeks.com/apis/fake/todos/user/elizaduarte', requestOptions)
-        .then( (response) => response.json() )
-        .then( (data) => console.log(data) )
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ task: newTask }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            setTasks([...tasks, data]);
+            setNewTask('');
+          });
       }
-
-      function getTask () {
-        console.log('leer tareas')
-        fetch('https://playground.4geeks.com/apis/fake/todos/user/elizaduarte')
-        .then( (response) => response.json() )
-        .then( (data) => console.log(data) )
-      }
-
-      function addTask (e) {
-          e.preventDefault();  
-          //setTasksList('');
-          console.log(tasksList)
-          console.log(task)
-
-          const requestOptions = {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(
-              [{ task }]
-            )
-        }
-          
-          console.log('Agregar tarea')
-          fetch('https://playground.4geeks.com/apis/fake/todos/user/elizaduarte', requestOptions)
-          .then( (response) => response.json() )
-          .then( (data) => console.log(data) )
-
-          
-        }
-
-      const deleteTask = (index) => {     
-
-          const newList = [...task];
-          newList.splice(index,1);
-          setTasks(newList);
-          
-
-          const requestOptions = {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(
-              
-               newList
-              
-            )
-          };
-          
-          fetch('https://playground.4geeks.com/apis/fake/todos/user/elizaduarte', requestOptions)
-          .then( (response) => response.json() )
-          .then( (data) => console.log(data) )
-        
-      }
-
+    };
+  
+    const deleteTask = () => {
+      fetch(`https://playground.4geeks.com/apis/fake/todos/user/elizaduarte`, {
+        method: 'DELETE',
+      }).then(() => {
+        const updatedTasks = tasks.filter((task) => task !== task);
+        setTasks(updatedTasks);
+      });
+    };
 
     return (
         <div className="container">
@@ -80,19 +46,19 @@ const Home =() => {
                         type="text"
                         className="form-control"
                         placeholder="What needs to be done?"
-                        value={task}
-                        onChange={e => getTask(e)}
+                        value={newTask}
+                        onChange={(e) => setNewTask(e.target.value)}
                         onKeyUp={e => addTask(e)}
                     />
                 </div>
                 <ul className="list-group col-8 p-0">
-                    {tasksList.map((task, index) => (
+                    {tasks.map((tasks, index) => (
                         <li
                             key={index}
                             className="list-group-item d-flex justify-content-between">
-                            <span>{task}</span>
+                            <span>{tasks}</span>
                             <button
-                                onClick={_e => deleteTask(index)}
+                                onClick={() => deleteTask(index)}
                                 style={{ border: 'none', background: 'none', padding: 0, cursor: 'pointer' }}
                             >
                                 <i className="far fa-trash-alt"></i>
@@ -100,9 +66,9 @@ const Home =() => {
                         </li>
                     ))}
                 </ul>
-                {tasksList.length > 0 ? (
+                {setNewTask.length > 0 ? (
                     <div className="note">
-                        {tasksList.length} item{tasksList.length === 1 ? '' : 's'} left
+                        {setNewTask.length} item{setNewTask.length === 1 ? '' : 's'} left
                     </div>
                 ) : (
                     <div>
